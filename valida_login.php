6241ac401d98d43662c5eddf7484f8803bce3015
Felipe Cardoso <felipedclc@gmail.com>
2020-09-30 19:08:35 -0300

    pag de validacao e autenticacao do usuario

---
+++ valida_login.php
@@ -0,0 +1,38 @@
+<?php
+
+$usuario_autenticado = false;
+
+$usuarios_app = [  // cadastro de usuarios do sistema
+    array('email' => 'adm@teste.com', 'senha' => '12345'),
+    array('email' => 'user@teste.com', 'senha' => '102030')
+];
+
+foreach($usuarios_app as $user) {
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) { // se o post for igual ao cadastro
+        $usuario_autenticado = true;
+    }
+}
+
+if($usuario_autenticado) {
+    print 'Usuario autenticado';
+}
+else {
+    header('Location: index.php?login=erro'); // desviando a aplicação para login=erro
+}
+
+
+/*
+    print_r($_GET);
+    print '<br>';
+    print $_GET['email'];
+    print '<br>';
+    print $_GET['senha'];
+
+    print_r($_POST);
+    print '<br>';
+    print $_POST['email'];
+    print '<br>';
+    print $_POST['senha'];
+*/
+    
+?>
