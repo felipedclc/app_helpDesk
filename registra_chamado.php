793d24989f33af1413c41ad8d967bc6197b66e54
Felipe Cardoso <felipedclc@gmail.com>
2020-10-02 10:39:47 -0300

    Registrando as chamadas e salvando em arquivo.txt

---
+++ registra_chamado.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo '<pre>';
+    print_r($_POST);
+
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    $texto = $_POST['titulo']. '#'. $_POST['categoria']. '#'. $_POST['descricao'] . PHP_EOL; // PHP_EOL (passa o proximo aruivo para linha de baixo)
+
+    // $texto_array = [$_POST['titulo'], $_POST['categoria'], $_POST['descricao']];
+    // $texto_string = implode("//", $texto_array);
+    
+    $arquivo = fopen('arquivo.txt', 'a'); // abre um novo arquivo - a(arquivo somente para leitura)    
+
+    fwrite($arquivo, $texto); // acrescenta o $texto_string no final do $arquivo 
+
+    fclose($arquivo); // fecha o arquivo 
+
+    header('Location: abrir_chamado.php')
+
+?>
